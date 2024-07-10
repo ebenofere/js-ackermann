4ca2eec72ef739656bebe15a4fc95fd4721278c5
ebenofere <eofere@gmail.com>
2024-07-10 13:36:44 -0400

    update

--- Practice/12. NestedCountingLoop.js
+++ Practice/12. NestedCountingLoop.js
@@ -12,22 +12,43 @@
 
 
 
+
 // Iteration over a multidimensional array using nested counting loops
-const coordinates = [
-    [50.69, 8.94],
-    [54.29, 11.97],
-    [55.38, 12.88],
-    [60.78, 9.94],
-    [62.29, 7.69],
-    [70.34, 8.24],
-    [2.59, 11.29],
-    [80.69, 8.56]
-];
+// const coordinates = [
+//     [50.69, 8.94],
+//     [54.29, 11.97],
+//     [55.38, 12.88],
+//     [60.78, 9.94],
+//     [62.29, 7.69],
+//     [70.34, 8.24],
+//     [2.59, 11.29],
+//     [80.69, 8.56]
+// ];
 
-for (let i = 0; i < coordinates.length; i++) {
-    const coordinate = coordinates[i];
+// for (let i = 0; i < coordinates.length; i++) {
+//     const coordinate = coordinates[i];
 
-    for (let j = 0; j < coordinate.length; j++) {
-        console.log(coordinate[j]);
+//     for (let j = 0; j < coordinate.length; j++) {
+//         console.log(coordinate[j]);
+//     }
+// }
+
+
+
+
+
+// An example of how loops can be aborted via the break keyword
+const numbers = [2, 4, 56, 22, 65, 2, 54, 88, 29];
+console.log("Before the loop");
+
+for(let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    if(number % 2 === 1) {
+        console.log("Odd number found.");
+        break;
     }
+
+    console.log(number);
 }
+console.log("After the loop");
