0812f52f701418d981be2276ab8a16f9b821a468
ebenofere <eofere@gmail.com>
2024-09-26 03:08:50 -0400

    update

--- practice.js
+++ practice.js
@@ -1,22 +1,19 @@
-// this nested loop finds duplicate numbers in the array
-const numbers = [2, 4, 56, 22, 65, 2, 54, 88, 29];
-console.log("Before the loop");
+// jump labels can be used to specify the target of a continue statement. the continue outerloop statement then ensures that if a duplicate number is found, the process specifiically jumps from the inner loop to the outer loop and continues with the next loop iteration of this outer loop.
 
-// inner loop
+const numbers = [2, 4, 56, 22, 65, 2, 54, 88, 29];
+
+outerLoop:
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
 
-    // outer loop
+    innerLoop:
     for (let j = i + 1; j < numbers.length; j++) {
         const number2 = numbers[j];
-        console.log(`compare ${number} to ${number2}`);
-        
+
+        console.log(`compare ${number} with ${number2}`);
         if (number === number2) {
-            console.log("both numbers are the same");
-            continue;
+            console.log("Same numbers found");
+            break outerLoop;
         }
     }
-
 }
-
-console.log("After the loop");
